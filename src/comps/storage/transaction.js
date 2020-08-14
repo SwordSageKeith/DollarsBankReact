5e4820426ff8ad40d7af113012e383477afc1b7a
Keith Prince <keithcprince@gmail.com>
2020-08-14 13:09:39 -0400

    started making transaction history

---
+++ src/comps/storage/transaction.js
@@ -0,0 +1,31 @@
+
+
+class trans {
+  username;
+  password;
+  id;
+  country;
+  phone;
+  balance;
+
+  constructor (username, password, id, country, phone, balance){
+    this.username = username;
+    this.password = password;
+    this.id = id;
+    this.country = country;
+    this.phone = phone;
+    this.balance = balance;
+  }
+
+  makeDeposit(i){
+    this.balance += parseFloat(i);
+    alert("new balance is " + this.balance);
+    
+  }
+  makeWithdraw(i){
+    this.balance -= parseFloat(i);
+    alert("new balance is " + this.balance);
+  }
+}
+
+export default trans;
